c0d7288a2cf2b9d976af81882966f5f820ed6d21
Admir <adopilot@gmail.com>
2024-04-26 14:16:26 +0200

    sada smo

---
+++ EsirDriver/EsirDriverEngin.cs
@@ -0,0 +1,25 @@
+﻿using EsirDriver.Modeli;
+using Shared.Fiscal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsirDriver
+{
+    public class EsirDriverEngin
+    {
+        private EsirConfigModel _esirConfig;
+        HttpClient _httpClient;
+        public EsirDriverEngin(EsirConfigModel esirConfigModel) 
+        { 
+            _esirConfig = esirConfigModel; 
+            _httpClient = new HttpClient() { BaseAddress = new Uri(esirConfigModel?.EsirSettingsModel.webserverAddress ?? "http://127.0.0.1:3566/")};
+        }
+
+
+
+
+    }
+}
---
+++ EsirDriver/Modeli/PrevoditeljSettingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsirDriver.Modeli
+{
+    public class PrevoditeljSettingModel
+    {
+        public bool KoristiSe { get; set; } = false;
+        public bool Upaljen { get; set; } = false;
+        public EsirConfigModel EsirConfig { get; set; } = new EsirConfigModel() { EsirSettingsModel = new EsirSettingsModel() };
+
+
+    }
+}
